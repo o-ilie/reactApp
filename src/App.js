7d632f52f77889c5a306ec2de8701d829767d2f7
o-ilie <ilie.oana27@gmail.com>
2021-11-24 17:04:37 +0200

    gilter

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import Expenses from "./components/Expenses";
+import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 
 function App() {
 	const expenses = [
@@ -23,8 +24,13 @@ function App() {
 		},
 	];
 
+	const addExpenseHandler = (expense) => {
+		console.log(expense);
+	};
+
 	return (
 		<>
+			<NewExpense onAddExpenseHandler={addExpenseHandler} />
 			<Expenses items={expenses} />
 		</>
 	);
